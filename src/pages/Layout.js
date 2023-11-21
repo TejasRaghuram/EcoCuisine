c74991a6f5260b9679fae5e173106da598a88090
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-20 23:38:13 -0500

    Added Page Routing

---
+++ src/pages/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+    return (
+        <div>
+            <p>This is a Header</p>
+            <Outlet/>
+            <p>This is a Footer</p>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+    return (
+        <div>
+            <p>This is the Register Page</p>
+        </div>
+    );
+}
+
+export default Register;
