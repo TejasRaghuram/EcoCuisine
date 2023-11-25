c592abc069f64cdc379af9c71bbeb37fcdab43e2
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-25 17:59:13 -0500

    Created createToken(id, metaData)
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,128 +1,31 @@
-import React from "react";
-import { getRestaurants, getMenu, getOrderPrice, getDeliveryFee, getDeliveryTime, getOrderEcoScore, getAwardedEcoPoints } from "./../scripts/RestaurantData.js";
+import React, { useState } from 'react';
 import { createToken, getEcoPoints, addEcoPoints, subtractEcoPoints, getDiscount } from "./../scripts/EcoPoints.js";
 
-function itemsRender(order)
-{
-    let result = "";
-    for(let i = 0; i < order.length; i++)
-    {
-        result += JSON.stringify(order[i]) + "\n";
-    }
-    return result;
-}
-
 function Test() {
-    
-    //EDIT PARAMETERS FOR RESTAURANTDATA HERE
-    let restaurant = "N/A";
-    let order = [{name: "N/A", cost: 0, time: 0, ecoscore: 0}];
-    let currentAddress = "N/A";
-    let restaurantAddress = "N/A";
-    let ecoscore = 0;
-    let price = 0.00;
+    const [tokenResult, setTokenResult] = useState('Click start to create a token');
+    const [ecoPoints, setEcoPoints] = useState('EcoPoints data will appear here');
+    const [updatedEcoPoints, setUpdatedEcoPoints] = useState('Updated EcoPoints data will appear here');
+    const [discountResult, setDiscountResult] = useState('Discount data will appear here');
 
-    //EDIT PARAMETERS FOR ECOPOINTS HERE
-    let email = "N/A";
-    let add_amount = 0;
-    let subtract_amount = 0;
-    let ecopoints = 0;
+    const handleCreateToken = () => {
+        setTokenResult('Creating token...');
+        createToken("TestToken")
+            .then(result => setTokenResult(`Token created: ${result}`))
+            .catch(error => setTokenResult(`Token creation failed: ${error.message}`));
+    };
+
+    // Handlers for other functions can be added here
 
     return (
         <div>
-            <br></br>
-            <p style={{fontWeight: "bold", textDecoration: "underline"}}>RestaurantData Functions</p>
-            <br></br>
+            <h1>Testing EcoPoints Functions</h1>
+            <button onClick={handleCreateToken}>Start</button>
+            <p>{tokenResult}</p>
 
-            <p style={{fontWeight: "bold"}}>Parameters</p>
-            <p style={{textDecoration: "underline"}}>restaurant: </p>
-            <p>{restaurant}</p>
-            <p style={{textDecoration: "underline"}}>order: </p>
-            <p>{itemsRender(order)}</p>
-            <p style={{textDecoration: "underline"}}>currentAddress: </p>
-            <p>{currentAddress}</p>
-            <p style={{textDecoration: "underline"}}>restaurantAddress: </p>
-            <p>{restaurantAddress}</p>
-            <p style={{textDecoration: "underline"}}>ecoscore: </p>
-            <p>{ecoscore}</p>
-            <p style={{textDecoration: "underline"}}>price: </p>
-            <p>{price}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getRestaurants</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getRestaurants()}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getMenu</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{itemsRender(getMenu(restaurant))}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getOrderPrice</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getOrderPrice(order)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getDeliveryFee</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getDeliveryFee(currentAddress, restaurantAddress)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getDeliveryTime</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getDeliveryTime(currentAddress, restaurantAddress, order)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getOrderEcoScore</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getOrderEcoScore(currentAddress, restaurantAddress, order)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getEcoPoints</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getAwardedEcoPoints(ecoscore, price)}</p>
-            <br></br>
-
-            <br></br>
-            <p style={{fontWeight: "bold", textDecoration: "underline"}}>EcoPoints Functions</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>Parameters</p>
-            <p style={{textDecoration: "underline"}}>email: </p>
-            <p>{email}</p>
-            <p style={{textDecoration: "underline"}}>amount(for addEcoPoints): </p>
-            <p>{add_amount}</p>
-            <p style={{textDecoration: "underline"}}>amount(for subtractEcoPoints): </p>
-            <p>{subtract_amount}</p>
-            <p style={{textDecoration: "underline"}}>ecopoints: </p>
-            <p>{ecopoints}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>createToken</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{createToken(email).toString()}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getEcoPoints</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getEcoPoints(email)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>addEcoPoints</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{addEcoPoints(email, add_amount)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>subtractEcoPoints</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{subtractEcoPoints(email, subtract_amount)}</p>
-            <br></br>
-
-            <p style={{fontWeight: "bold"}}>getDiscount</p>
-            <p style={{textDecoration: "underline"}}>Method Result:</p>
-            <p>{getDiscount(ecopoints)}</p>
-            <br></br>
+            {/* Placeholder for results of other functions */}
+            <p>{ecoPoints}</p>
+            <p>{updatedEcoPoints}</p>
+            <p>{discountResult}</p>
         </div>
     );
 }
--- src/scripts/EcoPoints.js
+++ src/scripts/EcoPoints.js
@@ -1,3 +1,83 @@
+// Variables
+const myAccountId = "0.0.5947923";
+const myPrivateKey = "3030020100300706052b8104000a04220420e9c5a05be4b1dcd3e722fba6b450b0da72b99c8783e072c1a37fbaa52a1e24f5";
+const { 
+    Client, 
+    PrivateKey, 
+    TokenCreateTransaction, 
+    TokenType, 
+    TokenSupplyType, 
+    Hbar 
+} = require("@hashgraph/sdk");
+const client = Client.forTestnet();
+
+client.setOperator(myAccountId, PrivateKey.fromString(myPrivateKey));
+client.setDefaultMaxTransactionFee(new Hbar(100));
+
+// Functions
+function createToken(id, metaData) {
+    const tokenCreateTx = new TokenCreateTransaction()
+        .setTokenName(id)
+        .setTokenSymbol("ID")
+        .setTokenType(TokenType.NonFungibleUnique) // Set the token type to Non-Fungible Unique
+        .setSupplyType(TokenSupplyType.Finite) // Set the supply type to finite
+        .setMaxSupply(1) // Set max supply to 1 for uniqueness
+        .setTreasuryAccountId(myAccountId)
+        .setAdminKey(PrivateKey.fromString(myPrivateKey).publicKey)
+        .setSupplyKey(PrivateKey.fromString(myPrivateKey).publicKey)
+        .setTokenMemo(metaData) // Set metadata as token memo (Note: This is a simple string field)
+        .freezeWith(client);
+
+    return tokenCreateTx.sign(PrivateKey.fromString(myPrivateKey))
+        .then(signedTx => signedTx.execute(client))
+        .then(txResponse => txResponse.getReceipt(client))
+        .then(receipt => {
+            if (receipt.status.toString() === "SUCCESS") {
+                return receipt.tokenId.toString();
+            } else {
+                throw new Error(`Token creation failed with status: ${receipt.status}`);
+            }
+        })
+        .catch(error => {
+            console.error("Error creating token:", error);
+            throw error;
+        });
+}
+
+
+// This function will return the amount of EcoPoints a user(token) has, given the user's email
+function getEcoPoints(email)
+{
+    return 0;
+}
+
+// This function will add EcoPoints to a user's corresponding token, given the user's email and the amount of EcoPoints to be added
+// It will return the resulting amount of EcoPoints the user has
+function addEcoPoints(email, amount)
+{
+    return 0;
+}
+
+// This function will subtract EcoPoints to a user's corresponding token, given the user's email and the amount of EcoPoints to be added
+// It will return the resulting amount of EcoPoints the user has
+function subtractEcoPoints(email, amount)
+{
+    return 0;   
+}
+
+/**
+ * Calculates the discount based on the given eco points.
+ * @param {number} ecopoints - The number of eco points.
+ * @returns {number} The discount amount.
+ */
+function getDiscount(ecopoints)
+{
+    return ecopoints * 0.025;
+}
+
+export { createToken, getEcoPoints, addEcoPoints, subtractEcoPoints, getDiscount };
+
+
 /*
 
 The functions in this file are to be implemented by Vignesh Saravanakumar
@@ -33,39 +113,3 @@ Have fun!
 
 // This function will create a token for a user given their email, and sets initial EcoPoints metadata to 0
 // It will return a boolean value that represents success or failure
-function createToken(email)
-{
-    return false;
-}
-
-// This function will return the amount of EcoPoints a user(token) has, given the user's email
-function getEcoPoints(email)
-{
-    return 0;
-}
-
-// This function will add EcoPoints to a user's corresponding token, given the user's email and the amount of EcoPoints to be added
-// It will return the resulting amount of EcoPoints the user has
-function addEcoPoints(email, amount)
-{
-    return 0;
-}
-
-// This function will subtract EcoPoints to a user's corresponding token, given the user's email and the amount of EcoPoints to be added
-// It will return the resulting amount of EcoPoints the user has
-function subtractEcoPoints(email, amount)
-{
-    return 0;   
-}
-
-/**
- * Calculates the discount based on the given eco points.
- * @param {number} ecopoints - The number of eco points.
- * @returns {number} The discount amount.
- */
-function getDiscount(ecopoints)
-{
-    return ecopoints * 0.025;
-}
-
-export { createToken, getEcoPoints, addEcoPoints, subtractEcoPoints, getDiscount };
