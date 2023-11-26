6ba7a83b816692929fa31e11ab751665eee42f40
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-25 21:15:07 -0500

    Removed Unnecessary Comments
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/scripts/EcoPoints.js
+++ src/scripts/EcoPoints.js
@@ -186,37 +186,3 @@ function getDiscount(ecopoints)
 }
 
 export { createToken, getEcoPoints, addEcoPoints, subtractEcoPoints, getDiscount };
-
-
-/*
-
-The functions in this file are to be implemented by Vignesh Saravanakumar
-
-You will need to download Node if you haven't already(https://nodejs.org/en/download)
-
-Run the project by executing "npm start" in the terminal
-Ctrl-C to terminate the project
-
-Go to http://localhost:3000/test to test out the functions
-The parameters can be edited in pages/Test.js
-
-Start by creating a new branch called "ecopoints-dev"
-Commit often, and add concise, professional commit messages
-They should have all "important" words capitalized, such as the following:
-"Implemented the getData Function"
-You can see the messages of existing commits for more examples
-
-This Hedera Node tutorial is a great place to begin
-https://www.youtube.com/watch?v=JZDAMScxbpU
-
-Specific instructions detailing what each function does can be found above the function
-Delete them after completing the corresponding function
-
-You may have to modify the functions a little bit so that they work with Hedera and blockchain
-Make sure the main idea is still there if you do\
-
-Delete this message after the work is done, just before merging
-
-Have fun!
-
-*/
