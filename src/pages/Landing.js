75b9f5b35081dc4e86327137b20ccac6ad9ff2b1
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-27 13:33:15 -0500

    Added Image to Landing Page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LandingImage from "./../images/landing.png";
 
 function Landing() {
     return (
         <div>
             <div class="spacing" />
             <h1 id="landing_header">Welcome<br/>to<br/>EcoCuisine!</h1>
+            <img src={LandingImage} id="landing_image" alt=""></img>
             <button onClick={()=>{}} class="button" id="landing_button">GET STARTED</button>
         </div>
     );
