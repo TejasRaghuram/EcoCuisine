775232eb86c3dd7fa5a52a120cf5201a13a66ed6
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-26 22:55:04 -0500

    Finished Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,8 +3,10 @@ import React from "react";
 function Error() {
     return (
         <div>
-            <div class="spacing" />
-            <p>Error 404: Page Not Found</p>
+            <div class="spacing"/>
+            <div id="error_container">
+                <h1 id="error">Error 404: Page Not Found</h1>
+            </div>
         </div>
     );
 }
