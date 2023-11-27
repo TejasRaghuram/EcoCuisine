c94638600690429480cef1e690b2b3d5c43a110f
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-26 23:20:08 -0500

    Made Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,20 @@ function Login() {
     return (
         <div>
             <div class="spacing" />
-            <p>This is the Login Page</p>
+            <form action={() => {}} class="auth_container">
+                <h3>Login</h3>
+                <label for="fname" class="auth_label">USERNAME:</label>
+                <br/>
+                <input type="text" id="username" name="fname" class="auth_input"/>
+                <br/>
+                <br/>
+                <label for="lname" class="auth_label">PASSWORD:</label>
+                <br/>
+                <input type="password" id="password" name="lname" class="auth_input"/>
+                <br/>
+                <br/>
+                <input type="submit" value="Submit" class="auth_input"/>
+            </form>
         </div>
     );
 }
