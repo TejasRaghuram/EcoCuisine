826ea74f981e535271013a78ea711c3371336195
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-26 23:55:09 -0500

    Created Landing Page + Adjusted Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,7 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
+          <Route index element={<Landing/>}/>
+          <Route path="home" element={<Home/>}/>
           <Route path="login" element={<Login/>}/>
           <Route path="register" element={<Register/>}/>
           <Route path="test" element={<Test/>}/>
---
+++ src/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <div>
+            <div class="spacing" />
+            <p>This is the Landing Page</p>
+        </div>
+    );
+}
+
+export default Landing;
