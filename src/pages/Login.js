8468cb92e44b4928c6402131b6c8718f7b3442bc
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-26 23:49:13 -0500

    Styled Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 function Login() {
     return (
         <div>
             <div class="spacing" />
             <form action={() => {}} class="auth_container">
-                <h3>Login</h3>
-                <label for="fname" class="auth_label">USERNAME:</label>
+                <h3 class="auth_header">Login</h3>
+                <label for="email" class="auth_label">EMAIL:</label>
                 <br/>
-                <input type="text" id="username" name="fname" class="auth_input"/>
-                <br/>
-                <br/>
-                <label for="lname" class="auth_label">PASSWORD:</label>
-                <br/>
-                <input type="password" id="password" name="lname" class="auth_input"/>
+                <input type="text" name="email" class="auth_input"/>
                 <br/>
                 <br/>
-                <input type="submit" value="Submit" class="auth_input"/>
+                <label for="password" class="auth_label">PASSWORD:</label>
+                <br/>
+                <input type="password" name="password" class="auth_input"/>
+                <br/>
+                <br/>
+                <input type="submit" value="Submit" class="button auth_button"/>
+                <br/>
+                <br/>
+                <Link to="./../Register" class="auth_link_label">Not a member? <span class="auth_link">Sign Up</span></Link>
             </form>
+            <div class="auth_bottom_spacing" />
         </div>
     );
 }
