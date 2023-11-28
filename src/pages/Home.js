1f3164062ff67a68b1e7bc678cfbf20dd6322f57
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-27 23:52:13 -0500

    Added Home Section Headers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,25 @@ function Home() {
             <div class="spacing" />
             <h1>Welcome!</h1>
             <h3 id="home_subtitle">Please place your order below</h3>
+            <div class="home_section_header">
+                <h3 class="home_section_header_number">1</h3>
+                <h3 class="home_section_header_text">Select Restaurant</h3>
+            </div>
+            <br/>
+            <div class="home_section_header">
+                <h3 class="home_section_header_number">2</h3>
+                <h3 class="home_section_header_text">Place Order</h3>
+            </div>
+            <br/>
+            <div class="home_section_header">
+                <h3 class="home_section_header_number">3</h3>
+                <h3 class="home_section_header_text">Review Order</h3>
+            </div>
+            <br/>
+            <div class="home_section_header">
+                <h3 class="home_section_header_number">4</h3>
+                <h3 class="home_section_header_text">Order Confirmation</h3>
+            </div>
         </div>
     );
 }
