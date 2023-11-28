4a2d75e129affdbf62ab350799e5c1b1a9795c63
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-27 23:15:09 -0500

    Finished Landing Page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,8 +8,6 @@ function Landing() {
             <h1 id="landing_header">Welcome<br/>to<br/>EcoCuisine!</h1>
             <img src={LandingImage} id="landing_image" alt=""></img>
             <button onClick={()=>{}} class="button" id="landing_button">GET STARTED</button>
-            <img src={LandingImage} id="landing_image" alt=""></img>
-            <img src={LandingImage} id="landing_image" alt=""></img>
         </div>
     );
 }
