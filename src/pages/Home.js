ca9c4b0b4920eb97239869d1ca0f94172afbdf98
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-29 20:16:58 -0500

    Home Page Order Review Section Completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,9 +48,6 @@ function Home() {
                 <h3 class="home_section_prompt">Choose Your Restaurant:</h3>
                 <Select options={restaurants} class="home_section_input" />
                 <p class="home_section_confirmation_header">Your Selection is: <span class="home_section_confirmation_text">Please Select a Valid Restaurant</span></p>
-                <h3 class="home_section_prompt">Enter Your Address:</h3>
-                <input type="text" class="home_section_input"></input>
-                <p class="home_section_confirmation_header">Your Selection is: <span class="home_section_confirmation_text">Please Enter a Valid Address</span></p>
                 <button class="button home_section_next">NEXT</button>
             </div>
             <br/>
@@ -65,18 +62,40 @@ function Home() {
                 <h3 class="home_section_prompt" style={{height: '60px'}}>Your Order:</h3>
                 {order}
                 <br />
-                <h3 class="home_section_prompt">Total Price: $0</h3>
-                <button class="button home_section_next">NEXT</button>
+                <button class="button home_section_next" style={{margin: '0'}}>NEXT</button>
             </div>
             <br/>
             <div class="home_section_header">
                 <h3 class="home_section_header_number">3</h3>
                 <h3 class="home_section_header_text">Review Order</h3>
             </div>
+            <div class="home_section">
+                <h3 class="home_section_prompt" style={{height: '60px'}}>Your Order:</h3>
+                {order}
+                <br />
+                <h3 class="home_section_prompt">Total Price: $0</h3>
+                <h3 class="home_section_prompt">Delivery Fee: $0</h3>
+                <br />
+                <p style={{fontSize: '18px'}}>You have 0 EcoPoints. Would you like to use them to get a $0 discount on your order?</p>
+                <button class="button home_section_next" style={{marginTop: '0'}}>YES</button>
+                <button class="button home_section_next" style={{marginTop: '0', marginLeft: '20px'}}>NO</button>
+            </div>
             <br/>
             <div class="home_section_header">
                 <h3 class="home_section_header_number">4</h3>
                 <h3 class="home_section_header_text">Order Confirmation</h3>
+            </div>
+            <div class="home_section">
+                <h3 class="home_section_prompt" style={{height: '60px'}}>Your Order:</h3>
+                {order}
+                <h3 class="home_section_prompt">Subtotal: $0</h3>
+                <h3 class="home_section_prompt">Delivery Fee: $0</h3>
+                <h3 class="home_section_prompt">EcoPoints Discount: $0</h3>
+                <h3 class="home_section_prompt">Estimated Delivery Time: 0 minutes</h3>
+                <br />
+                <p style={{fontSize: '18px'}}>The EcoScore of your order is 0.</p>
+                <p style={{fontSize: '18px'}}>You have been awarded 0 EcoPoints.</p>
+                <p style={{fontSize: '24px', fontWeight: 'bold'}}>Thanks for dining with EcoCuisine!</p>
             </div>
             <button class="button" id="logout" onClick={() => {}}>LOG OUT</button>
         </div>
