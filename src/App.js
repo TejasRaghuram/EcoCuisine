03f23ae7b7cb3b17e44793a8b9570c64c75ef64b
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-29 22:48:26 -0500

    Deleted Custom Log In and Register Pages for Auth0

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Error from "./pages/Error";
 import Test from "./pages/Test";
 import './App.css';
@@ -17,8 +15,6 @@ function App() {
           <Route path="/" element={<Layout/>}>
             <Route index element={<Landing/>}/>
             <Route path="home" element={<Home/>}/>
-            <Route path="login" element={<Login/>}/>
-            <Route path="register" element={<Register/>}/>
             <Route path="test" element={<Test/>}/>
             <Route path="*" element={<Error/>}/>
           </Route>
